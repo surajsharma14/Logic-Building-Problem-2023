646f4aa5062423bf9e01d9b3ddcca762d771abbe
Suraj Kumar Sharma <72158521+surajsharma14@users.noreply.github.com>
2022-12-31 20:35:41 +0530

    Create to perform Addition, Multiplication.md

---
+++ Program to perform Addition, Multiplication.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c,d,e,f;
+    cout<<"Enter The Number:"<<endl;
+    cin>> a>>b;
+    c = a+b;
+    d = a-b;
+    e = a*b;
+    f = a/b;
+    cout << "Sum is :"<< c <<endl;
+    cout << "Subtraction is:"<<d<<endl;
+    cout << "Multiplication is:" <<e<<endl;
+    cout << "division is:" <<f<<endl;
+    return(0);
+
+}
