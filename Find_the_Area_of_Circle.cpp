fd854401edf0bd1991962015cfbfecc1fea11992
Suraj Kumar Sharma <72158521+surajsharma14@users.noreply.github.com>
2023-01-01 21:33:35 +0530

    Create Area of the circle.md

---
+++ Find_the_Area_of_Circle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#define PI 3.142
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    float R, A;
+    cout<<"Enter the value of R: ";
+    cin >> R;
+    A = PI * R * R;
+    cout<<" Area of Circle is "<< A;
+
+    return 0;
+}
