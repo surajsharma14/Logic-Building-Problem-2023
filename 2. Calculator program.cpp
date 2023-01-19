1a86a2f5cf5271a86b561c01de484fcdd3756491
Suraj Kumar Sharma <72158521+surajsharma14@users.noreply.github.com>
2023-01-19 21:00:58 +0530

    Calculator program.md

---
+++ 2. Calculator program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Enter The Two Numbers:"<<endl;
+    cin >>a>>b;
+    c=a+b;
+    cout << "Sum is:" << c << endl;
+    
+    return 0;
+}
