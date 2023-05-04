d90a81d58a6657fed8b851b80bf2ad85a2c4c7f7
Suraj Kumar Sharma <72158521+surajsharma14@users.noreply.github.com>
2023-05-04 10:33:04 +0530

    Circumference of circle.md

---
+++ 19. Program to Circumference Of Circle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int rad;
+  float PI = 3.14, ci;
+  cout<<"Enter the radius: ";
+  cin>>rad;
+  
+  ci = 2*PI*rad;
+  
+  cout<<"Circumference of Circle:"<<ci<<endl;
+  
+  return 0;
+  }
